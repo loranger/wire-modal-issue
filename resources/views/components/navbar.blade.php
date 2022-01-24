45a23b89da536321ba247758d5a279b2c6b66eaf
Laurent Goussard <loranger@free.fr>
2022-01-25 00:07:37 +0100

    :lipstick: sticky navbar

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,4 +1,4 @@
-<div class="w-full">
+<div class="w-full sticky top-0 bg-white shadow-xl z-10">
     <div x-data="{ open: false }" class="
           flex flex-col
           max-w-screen-xl
@@ -19,7 +19,7 @@
               transform
               tracking-relaxed
               lg:pr-8
-            "> wickedblocks </a>
+            "> modal demo </a>
             <button class="rounded-lg md:hidden focus:outline-none focus:shadow-outline" @click="open = !open">
                 <svg fill="currentColor" viewBox="0 0 20 20" class="w-8 h-8">
                     <path x-show="!open" fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z" clip-rule="evenodd"></path>
