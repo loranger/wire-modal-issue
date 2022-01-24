a0e1b31a72c4b5692dc310675b2abd39e853cb81
Laurent Goussard <loranger@free.fr>
2022-01-25 00:07:12 +0100

    :building_construction: Add vendor path for tailwind jit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
     content: [
+        "./vendor/wire-elements/modal/resources/views/*.blade.php",
         "./resources/**/*.blade.php",
         "./resources/**/*.js",
         "./resources/**/*.vue",
